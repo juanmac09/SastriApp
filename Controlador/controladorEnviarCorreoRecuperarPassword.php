1a373d343601b8d4342888170cf031fc99466fcb
Juan Romero <mateoromero0910@gmail.com>
2023-12-05 12:58:21 -0500

    Se actualizo el restablecimiento de password

--- Controlador/controladorEnviarCorreoRecuperarPassword.php
+++ Controlador/controladorEnviarCorreoRecuperarPassword.php
@@ -26,7 +26,7 @@ if ($resultado) {
     $datos['pass'] = $token;
     $obj -> modificarPassword($datos);
     $token = md5($token);
-    $link = "http://localhost/SastriApp/Vista/extras/cambiarContraseña/index.php?user=".$resultado['user_identificacion']."&token=".$token;
+    $link = "http://localhost/SastriApp/Vista/extras/cambiarContra/index.php?user=".$resultado['user_identificacion']."&token=".$token;
     $correo  -> enviarCorreoCambioPassword($resultado['user_correo'],$link);
 
     session_start();
